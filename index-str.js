5e23ec8c53de82351c746820e8993bae90d95b6f
Sahil R <sahilrw000@gmail.com>
2023-04-24 10:28:15 +0530

    Find the index of the first occurence in a string

---
+++ index-str.js
@@ -0,0 +1,12 @@
+// Description: 
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// my solution
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};

fa07f6a0ead92b2d335b259a708f7343384b11e7
Sahil R <sahilrw000@gmail.com>
2023-07-14 23:09:23 +0530

    move zeros

---
+++ move-zero.js
@@ -0,0 +1,34 @@
+// Description:
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+// Example 2:
+// Input: nums = [0]
+// Output: [0]
+
+// Constraints:
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
+
+// my solution:
+var moveZeroes = function (nums) {
+  let left = 0;
+  let right = 0;
+
+  while (right < nums.length) {
+    if (nums[right] !== 0) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+    }
+    right++;
+  }
+
+  for (let i = left; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+};

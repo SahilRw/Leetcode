662991f32babaac8217587ffade0e615e1fb2a6a
Sahil R <sahilrw000@gmail.com>
2023-11-17 20:14:44 +0530

    to lower case elaborate solution

--- lwr-cse.js
+++ lwr-cse.js
@@ -12,7 +12,27 @@
 // Input: s = "LOVELY"
 // Output: "lovely"
 
-// my solution(short version)
+// my solution(elaborate version)
 var toLowerCase = function(s) {
+    // declare an empty variable
+    let result = '';
+    // looping over the string
+    for (let i = 0; i < s.length; i++){
+        const charCode = s.charCodeAt(i)
+        // checking if character is uppercase
+        if(charCode >= 65 && charCode <= 90){
+            // convert to lowercase by adding ascii difference
+            result += String.fromCharCode(charCode + 32)
+        } else {
+            // keep the character as it is
+            result += s[i]
+        };
+    }
+    // return result
+    return result;
+};
+
+// my solution(short version)
+var toLowerCaseShort = function(s) {
     return s.toLowerCase()
 };
